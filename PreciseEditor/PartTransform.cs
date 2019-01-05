8f8c5b5731099f9545cf8b04c51ca16a1946cb2b
rouj1803 <Jean-Francois.Rouleau2@USherbrooke.ca>
2019-01-05 17:11:03 -0500

    Corrected mirror symmetry rotation bug.

--- PreciseEditor/PartTransform.cs
+++ PreciseEditor/PartTransform.cs
@@ -4,12 +4,6 @@ namespace PreciseEditor
 {
     public static class PartTransform
     {
-        /// <summary>
-        /// Set one of position component (x, y, z).
-        /// </summary>
-        /// <param name="part">Part to position</param>
-        /// <param name="newPosition">Part new position</param>
-        /// <param name="space">The reference space</param>
         public static void SetPosition(Part part, Vector3 newPosition, Space space)
         {
             Vector3 offset;
@@ -39,17 +33,9 @@ namespace PreciseEditor
             EditorLogic.fetch.SetBackup();
         }
 
-        /// <summary>
-        /// Set one of rotation component (x, y, z).
-        /// </summary>
-        /// <param name="part">Part to rotate.</param>
-        /// <param name="eulerAngles">The part new euler angles</param>
-        /// <param name="space">The reference space</param>
         public static void SetRotation(Part part, Vector3 eulerAngles, Space space)
         {
             Vector3 srcEulerAngles = (space == Space.Self) ? part.transform.localRotation.eulerAngles : part.transform.rotation.eulerAngles;
-            Vector3 deltaEuler = eulerAngles - srcEulerAngles;
-            Quaternion angle = Quaternion.Euler(deltaEuler);
 
             if (space == Space.Self)
             {
@@ -67,18 +53,13 @@ namespace PreciseEditor
             }
             else if (part.symMethod == SymmetryMethod.Radial)
             {
+                Quaternion angle = Quaternion.Euler(eulerAngles - srcEulerAngles);
                 PartTransform.UpdateRadialSymmetryCounterpartsRotation(part, angle);
             }
 
             EditorLogic.fetch.SetBackup();
         }
 
-        /// <summary>
-        /// Apply a rotation to a part and its symmetry counterparts.
-        /// </summary>
-        /// <param name="part">The part to rotate.</param>
-        /// <param name="eulerAngles">The rotation euler angles</param>
-        /// <param name="space">The reference space</param>
         public static void Rotate(Part part, Vector3 eulerAngles, Space space)
         {
             part.transform.Rotate(eulerAngles, space);
@@ -96,10 +77,6 @@ namespace PreciseEditor
             EditorLogic.fetch.SetBackup();
         }
 
-        /// <summary>
-        /// Update the position of every mirror symmetry counter part of a given part after it has been moved.
-        /// </summary>
-        /// <param name="part"></param>
         private static void UpdateMirrorSymmetryCounterpartsPosition(Part part)
         {
             if (part.parent)
@@ -117,11 +94,6 @@ namespace PreciseEditor
             }
         }
 
-        /// <summary>
-        /// Update the position of every radial symmetry counter part of a given part after it has been moved.
-        /// </summary>
-        /// <param name="part"></param>
-        /// <param name="offset">The translation vector.</param>
         private static void UpdateRadialSymmetryCounterpartsPosition(Part part, Vector3 offset)
         {
             foreach (Part symmetryCounterpart in part.symmetryCounterparts)
@@ -131,27 +103,21 @@ namespace PreciseEditor
             }
         }
 
-        /// <summary>
-        /// Update the rotation of every mirror symmetry counterpart of a given part after it has been rotated.
-        /// </summary>
-        /// <param name="part">The part that has rotated.</param>
         private static void UpdateMirrorSymmetryCounterpartsRotation(Part part)
         {
-            Quaternion rotation = part.transform.rotation;
-            Quaternion mirrorRotation = new Quaternion(-rotation.z, -rotation.w, -rotation.x, -rotation.y);
-
             foreach (Part symmetryCounterpart in part.symmetryCounterparts)
             {
-                symmetryCounterpart.transform.rotation = mirrorRotation;
+                symmetryCounterpart.transform.rotation = EditorGeometryUtil.MirrorRotation(part.transform.rotation, part.transform, EditorLogic.RootPart.transform);
+
+                if (part.attRotation0 == symmetryCounterpart.attRotation0)
+                {
+                    symmetryCounterpart.transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+                }
+
                 GameEvents.onEditorPartEvent.Fire(ConstructionEventType.PartRotated, symmetryCounterpart);
             }
         }
 
-        /// <summary>
-        /// Update the rotation of every radial symmetry counterpart of a given part after it has been rotated.
-        /// </summary>
-        /// <param name="part">The part that has rotated</param>
-        /// <param name="angle">The rotation to apply</param>
         private static void UpdateRadialSymmetryCounterpartsRotation(Part part, Quaternion rotation)
         {
             foreach (Part symmetryCounterpart in part.symmetryCounterparts)
@@ -161,12 +127,6 @@ namespace PreciseEditor
             }
         }
 
-        /// <summary>
-        /// Update the rotation of every radial symmetry counter part of a given part after it has been rotated.
-        /// </summary>
-        /// <param name="part"></param>
-        /// <param name="eulerAngle"></param>
-        /// <param name="space"></param>
         private static void RotateRadialSymmetryCounterparts(Part part, Vector3 eulerAngles, Space space)
         {
             if (space == Space.World)
